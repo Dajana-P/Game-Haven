ef703f7c33047d623dab11683c659c644e6f23c7
Dajana-P <daja002@gmail.com>
2024-03-11 21:49:34 +0100

    Initial commit

---
+++ game_haven/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:":ww_IGSBD[]tBr Q!A6>x@8a6V(r4X=##!b{jvy67$^i ~8lAH#Jq2RM/*pJ9PzF";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2512;}
